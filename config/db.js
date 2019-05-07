736c8311263c88dbf3ed3d5a9d5c326a368b7d65
Juan Simon <juansimon18.js@gmail.com>
2019-05-06 23:30:08 -0500

    feat add db config

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+
+const connectDB = async () => {
+    try {
+        const dbOptions = {
+            useNewUrlParser: true
+        }
+
+        await mongoose.connect(db, dbOptions)
+        console.log('connection successful')
+    } catch (e) {
+        console.log(`error connecting to mongo: ${e.message}`)
+        // exit node process with failure
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
